85b3ed3cf13478b5004ac733d5966017b9979693
Jonah Graham <jonah@kichwacoders.com>
2026-03-11 10:53:25 -0400

    Update composites for CDT 12.4 and CDT LSP 3.5 releases

--- releng/scripts/promote-files-to-download.sh
+++ releng/scripts/promote-files-to-download.sh
@@ -26,6 +26,7 @@ $ECHO $SCP ./releng/download/releases/12.0/* "${SSHUSER}:${DOWNLOAD}/releases/12
 $ECHO $SCP ./releng/download/releases/12.1/* "${SSHUSER}:${DOWNLOAD}/releases/12.1/"
 $ECHO $SCP ./releng/download/releases/12.2/* "${SSHUSER}:${DOWNLOAD}/releases/12.2/"
 $ECHO $SCP ./releng/download/releases/12.3/* "${SSHUSER}:${DOWNLOAD}/releases/12.3/"
+$ECHO $SCP ./releng/download/releases/12.4/* "${SSHUSER}:${DOWNLOAD}/releases/12.4/"
 
 $ECHO $SSH mkdir -p "${DOWNLOAD}/releases/cdt-lsp-latest/"
 $ECHO $SCP ./releng/download/releases/cdt-lsp-latest/* "${SSHUSER}:${DOWNLOAD}/releases/cdt-lsp-latest/"
@@ -35,6 +36,7 @@ $ECHO $SCP ./releng/download/releases/cdt-lsp-3.1/* "${SSHUSER}:${DOWNLOAD}/rele
 $ECHO $SCP ./releng/download/releases/cdt-lsp-3.2/* "${SSHUSER}:${DOWNLOAD}/releases/cdt-lsp-3.2/"
 $ECHO $SCP ./releng/download/releases/cdt-lsp-3.3/* "${SSHUSER}:${DOWNLOAD}/releases/cdt-lsp-3.3/"
 $ECHO $SCP ./releng/download/releases/cdt-lsp-3.4/* "${SSHUSER}:${DOWNLOAD}/releases/cdt-lsp-3.4/"
+$ECHO $SCP ./releng/download/releases/cdt-lsp-3.5/* "${SSHUSER}:${DOWNLOAD}/releases/cdt-lsp-3.5/"
 
 # Issue #235 - some of the old archived releases had bad composites. For those
 # ones we started maintaining the files here as it was the easiest way to edit
